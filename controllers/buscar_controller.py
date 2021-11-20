bf4d0671833b5412f7d7e476527242d853fe2f82
Cristiano <cristianoxavier_@outlook.com>
2021-11-20 15:46:16 -0300

    add geopy

--- controllers/buscar_controller.py
+++ controllers/buscar_controller.py
@@ -1,2 +1,10 @@
+from geopy.geocoders import Nominatim
+import folium
+
+
 def buscar_endereco(args):
-    return 'Teste'
+    locator = Nominatim(user_agent="myGeocoder")
+    endereco = args['endereco']
+    local = locator.geocode(endereco)
+    m = folium.Map(location=[local.latitude, local.longitude], zoom_start=13)
+    return m
